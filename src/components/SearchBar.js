0fa0eba26ff84cd9bcd33b547b38b6d6dd7d2c81
Chester Yap <chesterdyap@gmail.com>
2018-02-21 00:00:33 -0600

    Create search bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBar = () => (
+	<div>
+		<input
+			type="text"
+			placeholder="Enter subreddit name"
+		/>
+		<input
+			type="submit"
+		/>
+	</div>
+)
+
+export default SearchBar;
