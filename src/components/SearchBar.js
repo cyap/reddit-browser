8d26b7ebc1b35392439a1dfdd6555c0d7dc38b54
Chester Yap <chesterdyap@gmail.com>
2018-02-21 00:58:59 -0600

    Convert Search to component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
-const SearchBar = () => (
-	<div>
-		<input
-			type="text"
-			placeholder="Enter subreddit name"
-		/>
-		<input
-			type="submit"
-		/>
-	</div>
-)
+class SearchBar extends React.Component {
+	render() {
+		return (
+			<div>
+				<input
+					type="text"
+					placeholder="Enter subreddit name"
+				/>
+				<input
+					type="submit"
+				/>
+			</div>
+		)
+	}
+}
 
 export default SearchBar;
