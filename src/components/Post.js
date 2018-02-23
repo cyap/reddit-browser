b4f69bcba30a060d3e0c27c3d3201071e4bce5f5
Chester Yap <chesterdyap@gmail.com>
2018-02-23 02:09:04 -0600

    Add thumbnails

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import logo from '../logo.svg'
 
 class Post extends React.Component {
 	render() {
 		return (
 			<div className="post">
-				<div className="title">
-					{this.props.post.title}
+				<div className="titleBox">
+					<img className="thumbnail" src={this.props.post.thumbnail || logo} alt="?" className="thumbnail"/>
+					<span className="title">
+						{this.props.post.title}
+					</span>
 				</div>
 				<div>
 					<span className="author">Author: {this.props.post.author}</span>
--- src/utils/parse.js
+++ src/utils/parse.js
@@ -1,13 +1,21 @@
+import request from 'request';
+
 function jsonToPosts(json) {
 	let rawPosts = JSON.parse(json).data.children;
 	let posts = rawPosts.map(post => {
 		return {
 			title: post.data.title,
 			author: post.data.author,
-			score: post.data.score
+			score: post.data.score, 
+			thumbnail: validate_thumbnail(post)
 		}
 	})
 	return posts;
 }
+
+function validate_thumbnail(post) {
+	return post.data.thumbnail.endsWith(".jpg") ? post.data.thumbnail : null;
+}
+
 
 export default jsonToPosts;
