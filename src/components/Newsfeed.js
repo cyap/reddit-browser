56d9078b2289ead609b97dec6f533f26724b9a2b
Chester Yap <chesterdyap@gmail.com>
2018-02-23 05:16:48 -0600

    Info message for no posts

--- src/components/Newsfeed.js
+++ src/components/Newsfeed.js
@@ -5,6 +5,9 @@ class Newsfeed extends React.Component {
 	render() {
 		return (
 			<div>
+				<div className="infoMessage">
+					{this.props.posts.length ? '' : "No posts to display."}
+				</div>
 				{this.props.posts.map((post, i) => (
 						<Post 
 							key={i}
