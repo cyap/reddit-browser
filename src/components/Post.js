f3ad1060277a18719b510cae85085134efd4d8e1
Chester Yap <chesterdyap@gmail.com>
2018-02-23 03:40:23 -0600

    Add containers for post content

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,6 +2,12 @@ import React from 'react';
 import logo from '../logo.svg';
 
 class Post extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+      open: false
+    }
+	}
 	render() {
 		return (
 			<div className="post">
@@ -14,6 +20,9 @@ class Post extends React.Component {
 				<div>
 					<span className="author">Author: {this.props.post.author}</span>
 					<span className="score">Score: {this.props.post.score}</span>
+				</div>
+				<div className={"postContent" + (this.state.open ? '' : ' closed')}>
+					Foo
 				</div>
 			</div>
 		)
